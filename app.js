2b3675f470c05989d60b0f18205fd03805a32521
Bernardo Castro <bernardo.abreucastro@gmail.com>
2021-05-24 17:49:55 -0300

    Sign-in/up page

---
+++ app.js
@@ -0,0 +1,15 @@
+function changeToSignUp() {
+   document.querySelector(".js-name").style.display = "inherit" 
+   document.querySelector(".js-confirm").style.display = "inherit"
+   document.querySelector(".forgot-password").style.display = "none"
+   document.querySelector(".sign-in-text").textContent = "Sign Up"
+   document.querySelector(".sign-btn").value = "Sign up now"
+   document.querySelector(".sign-link-text").textContent = "Already have an account?"
+   document.querySelector(".sign-link").textContent = "Sign In"
+   document.querySelector(".sign-link").style.marginLeft = "235px"
+   document.querySelector("title").textContent = "Sign-up"
+}
+
+const linkSignUp = document.querySelector(".sign-link")
+linkSignUp.addEventListener('click', changeToSignUp)
+
